df751420637d02555f23867e87ba9bcf9b3731fe
DHN Chandan <php.chandan@gmail.com>
2021-06-20 08:19:29 +0600

    Test api gateway

--- lib/aws_cdk_crud-stack.ts
+++ lib/aws_cdk_crud-stack.ts
@@ -1,15 +1,21 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class AwsCdkCrudStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
 		// lambda function
-		new lambda.Function(this, "HelloHandler", {
+		const hello = new lambda.Function(this, "HelloHandler", {
 			runtime: lambda.Runtime.NODEJS_14_X,
 			code: lambda.Code.fromAsset("lambda"),
 			handler: "hello.handler",
+		});
+
+		// API gateway
+		new apigw.LambdaRestApi(this, "EndPoint", {
+			handler: hello,
 		});
 	}
 }
