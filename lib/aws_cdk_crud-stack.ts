a4a526a7a7eb7261eb1807c425412d4da9463bda
DHN Chandan <php.chandan@gmail.com>
2021-06-20 08:02:19 +0600

    Test hello lambda function

--- lib/aws_cdk_crud-stack.ts
+++ lib/aws_cdk_crud-stack.ts
@@ -1,9 +1,15 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class AwsCdkCrudStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+		// lambda function
+		new lambda.Function(this, "HelloHandler", {
+			runtime: lambda.Runtime.NODEJS_14_X,
+			code: lambda.Code.fromAsset("lambda"),
+			handler: "hello.handler",
+		});
+	}
 }
